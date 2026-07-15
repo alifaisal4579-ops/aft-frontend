3ac1ba9d0e0daef93140f2b3a63eb96ad9409581
alifaisal4579-ops <alifaisal4579@gmail.com>
2026-07-16 00:21:36 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import { AuthProvider } from './context/AuthContext.jsx';
-import './styles/global.css';
+import { AuthProvider } from './AuthContext.jsx';
+import './global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
